b2f68f17d7115705b8790b1d07f9adeb23844b85
slipet <slipet@github.com>
2024-07-11 22:44:45 +0800

    Move `__quick_sort()` from `main.c` to `list.c`
    To improve modularity and code organization, the `__quick_sort()`
    function has been relocated from `main.c` to `list.c`.

--- quiz1/list.c
+++ quiz1/list.c
@@ -49,3 +49,52 @@ void __list_free(node_t **list)
             node = node->next;
     }
 }
+
+
+void __quick_sort(node_t **list)
+{
+    int n = __list_length(list);
+    int value;
+    int i = 0;
+    int max_level = 2 * n;
+    node_t *begin[max_level], *end[max_level];
+    node_t *result = NULL, *left = NULL, *right = NULL;
+
+    begin[0] = *list;
+    end[0] = __list_tail(list);
+
+    while (i >= 0) {
+        node_t *L = begin[i], *R = end[i];
+
+        if (L != R) {
+            node_t *pivot = L;
+            value = pivot->value;
+            node_t *p = pivot->next;
+            pivot->next = NULL;
+
+            while (p) {
+                node_t *n = p;
+                p = p->next;
+                __list_add(n->value > value ? &right : &left, n);
+            }
+
+            begin[i] = left;
+            end[i] = __list_tail(&left);
+
+            begin[i + 1] = pivot;
+            end[i + 1] = pivot;
+
+            begin[i + 2] = right;
+            end[i + 2] = __list_tail(&right);
+
+            left = right = NULL;
+            i += 2;
+        } else {
+            if (L)
+                __list_add(&result, L);
+
+            i--;
+        }
+    }
+    *list = result;
+}
--- quiz1/main.c
+++ quiz1/main.c
@@ -40,55 +40,12 @@ void shuffle(int *array, size_t n)
     }
 }
 
-void quick_sort(node_t **list)
-{
-    int n = list_length(list);
-    int value;
-    int i = 0;
-    int max_level = 2 * n;
-    node_t *begin[max_level], *end[max_level];
-    node_t *result = NULL, *left = NULL, *right = NULL;
-
-    begin[0] = *list;
-    end[0] = list_tail(list);
-    while (i >= 0) {
-        node_t *L = begin[i], *R = end[i];
-        if (L != R) {
-            node_t *pivot = L;
-            value = pivot->value;
-            node_t *p = pivot->next;
-            pivot->next = NULL;
-
-            while (p) {
-                node_t *n = p;
-                p = p->next;
-                list_add(n->value > value ? &right : &left, n);
-            }
-
-            begin[i] = left;
-            end[i] = list_tail(&left);
-            begin[i + 1] = pivot;
-            end[i + 1] = pivot;            
-            begin[i + 2] = right;
-            end[i + 2] = list_tail(&right);
-
-            left = right = NULL;
-            i += 2;
-        } else {
-            if (L)
-                list_add(&result, L);
-            i--;
-        }
-    }
-    *list = result;
-}
-
 int main(int argc, char **argv)
 {
     node_t *list = NULL;
 
-    size_t count = 6;
-
+    size_t count = 1000;
+    size_t size = count;
     int *test_arr = malloc(sizeof(int) * count);
 
     for (int i = 0; i < count; ++i)
@@ -106,4 +63,5 @@ int main(int argc, char **argv)
     free(test_arr);
 
     return;
+
 }
